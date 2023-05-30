fba5e70589f3f419c6fad5e4343cdb9aaadf1987
Sushil <shil7276@gmail.com>
2023-05-30 15:45:16 +0530

    authentication role "admin" & forgot password

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const { getAllProduct, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
-const { isAuthenticateUser } = require("../middleware/auth");
+const { isAuthenticateUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router()
 
 
-router.route('/product').get(isAuthenticateUser, getAllProduct);
-router.route('/product/new').post(createProduct);
-router.route('/product/:id').put(updateProduct).delete(deleteProduct).get(getProductDetails);
+router.route('/product').get(getAllProduct);
+router.route('/product/new').post(isAuthenticateUser, authorizeRoles("admin"), createProduct);
+
+router.route('/product/:id')
+    .put(isAuthenticateUser, authorizeRoles("admin"), updateProduct)
+    .delete(isAuthenticateUser, authorizeRoles("admin"), deleteProduct)
+    .get(getProductDetails);
 
 
 module.exports = router;
