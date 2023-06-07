a88c981bd99d2b5754f41882a04f223f2912d44a
Sushil <shil7276@gmail.com>
2023-06-07 10:42:16 +0530

    getProduct not necessary login

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -11,7 +11,8 @@ router.route('/admin/product/new').post(isAuthenticateUser, authorizeRoles("admi
 router.route('/admin/product/:id')
     .put(isAuthenticateUser, authorizeRoles("admin"), updateProduct)
     .delete(isAuthenticateUser, authorizeRoles("admin"), deleteProduct)
-    .get(getProductDetails);
 
+
+router.route("/product/:id").get(getProductDetails);
 
 module.exports = router;
