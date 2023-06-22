f2f17b58fa27b8ee7597f54180e65786bff41352
Sushil <shil7276@gmail.com>
2023-06-22 20:04:42 +0530

    #Update Component created

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,22 +1,29 @@
-import { combineReducers, applyMiddleware, createStore } from "redux";
-// import { configureStore } from "@reduxjs/toolkit";
+// import { combineReducers, applyMiddleware, createStore } from "redux";
+import { combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension"
+// import { composeWithDevTools } from "redux-devtools-extension"
 import { productReducer, productDetailsReducer } from "./reducers/productReducer";
-import { userReducer } from "./reducers/userReducer";
+import { profileReducer, userReducer } from "./reducers/userReducer";
 
 
 const reducer = combineReducers({
     products: productReducer,
     productDetailsReducer,
-    user: userReducer
+    user: userReducer,
+    profile: profileReducer,
 });
 
 let initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+// const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+const store = configureStore({
+    reducer,
+    initialState,
+    middleware: [...middleware],
+})
 
 export default store;
 
