55f27b897c4f5e4cd425cd223cfc7cb2c8887137
Sushil <shil7276@gmail.com>
2023-06-22 20:09:53 +0530

    #update function add in user action

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -10,9 +10,9 @@ import {
     LOAD_USER_FAIL,
     LOGOUT_SUCCESS,
     LOGOUT_FAIL,
-    UPDATE_PROFILE_REQUEST,
-    UPDATE_PROFILE_SUCCESS,
-    UPDATE_PROFILE_FAIL,
+    // UPDATE_PROFILE_REQUEST,
+    // UPDATE_PROFILE_SUCCESS,
+    // UPDATE_PROFILE_FAIL,
     // UPDATE_PASSWORD_REQUEST,
     // UPDATE_PASSWORD_SUCCESS,
     // UPDATE_PASSWORD_FAIL,
@@ -123,29 +123,6 @@ export const logout = () => async (dispatch) => {
         dispatch({ type: LOGOUT_FAIL })
     }
 }
-
-
-// Update Profile  
-export const updateProfile = (userData) => async (dispatch) => {
-    try {
-
-        dispatch({ type: UPDATE_PROFILE_REQUEST });
-
-        const config = { headers: { "Content-Type": "multipart/form-data" } };
-
-        const { data } = await axios.put(`
-        ${url}/api/v1//me/update`,
-            userData,
-            config
-        );
-
-
-        dispatch({ type: UPDATE_PROFILE_SUCCESS, payload: data.user });
-
-    } catch (error) {
-        dispatch({ type: UPDATE_PROFILE_FAIL, payload: error.response.data.message })
-    }
-};
 
 
 // Clearing Errors
