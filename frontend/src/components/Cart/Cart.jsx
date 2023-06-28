93819bc70d13c4a7d6eaff028641d78fc9cd2704
Sushil <shil7276@gmail.com>
2023-06-28 20:33:52 +0530

    style - add style in cart

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -3,6 +3,9 @@ import './Cart.css'
 import CartItemCard from './CartItemCard'
 import { useDispatch, useSelector } from 'react-redux'
 import { addItemsToCart, removeCartItem } from '../../actions/cartAction';
+import { Typography } from "@material-ui/core";
+import RemoveShoppingCartIcon from "@material-ui/icons/RemoveShoppingCart";
+import { Link } from "react-router-dom";
 
 function Cart() {
 
@@ -29,7 +32,14 @@ function Cart() {
 
     return (
         <>
-            {cartItems.length === 0 ? "No Items in Cart" : (
+            {cartItems.length === 0 ? (
+                <div className="emptyCart">
+                    <RemoveShoppingCartIcon />
+
+                    <Typography>No Product in Your Cart</Typography>
+                    <Link to="/products">View Products</Link>
+                </div>
+            ) : (
                 <Fragment>
                     <div className="cartPage">
                         <div className="cartHeader">
