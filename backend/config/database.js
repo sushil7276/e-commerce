2fa462d8fbe85e9e8b0fa73fcd0d47718f5d0a79
Sushil <shil7276@gmail.com>
2023-07-21 17:47:24 +0530

    mongoose

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,14 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDatabase = () => {
     mongoose.set('strictQuery', false);
-    mongoose.connect(process.env.DB_URI, {
-
-        useNewUrlParser: "true",
-        useUnifiedTopology: "true"
-
-    }).then((data) => {
-        console.log(`Mongodb connected : ${data.connection.host}`)
-    })
+    mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then((data) => {
+            console.log(`Mongodb connected : ${data.connection.host}`)
+        })
+        .catch((error) => console.log(error.message))
+    mongoose.set('useFindAndModify', false)
 }
 
 module.exports = connectDatabase;
