fd90a6722af5ab3773e259ec369e802839a4daba
Sushil <shil7276@gmail.com>
2023-07-21 17:20:22 +0530

    mongoose change

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDatabase = () => {
+    mongoose.set('strictQuery', false);
     mongoose.connect(process.env.DB_URI).then((data) => {
         console.log(`Mongodb connected : ${data.connection.host}`)
     })
