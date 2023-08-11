9bcb65dc55ce5757812135d6b71aad83ccdd55fc
Sushil <shil7276@gmail.com>
2023-08-11 15:40:35 +0530

    about profile image and image instruction added

--- frontend/src/components/User/LoginSignUp.jsx
+++ frontend/src/components/User/LoginSignUp.jsx
@@ -1,234 +1,219 @@
-import React, { Fragment, useEffect, useRef, useState } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import "./LoginSignUp.css";
-import Loader from '../layout/Loader/Loader';
-import { Link } from 'react-router-dom';
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import LockOpenIcon from '@material-ui/icons/LockOpen';
-import FaceIcon from '@material-ui/icons/Face';
-import { useDispatch, useSelector } from 'react-redux';
-import { useAlert } from 'react-alert';
-import { clearErrors, login, register } from '../../actions/userAction';
-
+import Loader from "../layout/Loader/Loader";
+import { Link } from "react-router-dom";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import LockOpenIcon from "@material-ui/icons/LockOpen";
+import FaceIcon from "@material-ui/icons/Face";
+import { useDispatch, useSelector } from "react-redux";
+import { useAlert } from "react-alert";
+import { clearErrors, login, register } from "../../actions/userAction";
 
 function LoginSignUp({ history, location }) {
+  const dispatch = useDispatch();
+  const alert = useAlert();
 
-    const dispatch = useDispatch();
-    const alert = useAlert();
+  const { error, loading, isAuthenticated } = useSelector(
+    (state) => state.user
+  );
 
-    const { error, loading, isAuthenticated } = useSelector((state) => state.user);
+  const loginTab = useRef(null);
+  const registerTab = useRef(null);
+  const switchTab = useRef(null);
 
-    const loginTab = useRef(null);
-    const registerTab = useRef(null);
-    const switchTab = useRef(null);
+  // State
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
 
+  // user State
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-    // State
-    const [loginEmail, setLoginEmail] = useState("");
-    const [loginPassword, setLoginPassword] = useState("");
+  const { name, email, password } = user;
 
-    // user State
-    const [user, setUser] = useState({
-        name: "",
-        email: "",
-        password: "",
-    });
+  const [avatar, setAvatar] = useState();
+  const [avatarPreview, setAvatarPreview] = useState("/Profile.png");
 
-    const { name, email, password } = user;
+  // Login Event Handle
+  const loginSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login(loginEmail, loginPassword));
+  };
 
-    const [avatar, setAvatar] = useState();
-    const [avatarPreview, setAvatarPreview] = useState("/Profile.png");
+  // Register Event Handle
+  const registerSubmit = (e) => {
+    e.preventDefault();
 
+    const myForm = new FormData();
+    myForm.set("name", name);
+    myForm.set("email", email);
+    myForm.set("password", password);
+    myForm.set("avatar", avatar);
 
-    // Login Event Handle
-    const loginSubmit = (e) => {
-        e.preventDefault();
-        dispatch(login(loginEmail, loginPassword))
-    }
+    dispatch(register(myForm));
+  };
 
-    // Register Event Handle
-    const registerSubmit = (e) => {
-        e.preventDefault();
+  const registerDataChange = (e) => {
+    if (e.target.name === "avatar") {
+      const reader = new FileReader();
 
-        const myForm = new FormData();
-        myForm.set("name", name);
-        myForm.set("email", email);
-        myForm.set("password", password);
-        myForm.set("avatar", avatar);
-
-        dispatch(register(myForm));
-    }
-
-    const registerDataChange = (e) => {
-        if (e.target.name === 'avatar') {
-
-            const reader = new FileReader();
-
-            reader.onload = () => {
-
-                /* "(reader.readyState===0) `0` means initial Stage
+      reader.onload = () => {
+        /* "(reader.readyState===0) `0` means initial Stage
                     "(reader.readyState===1) `1` means loading Stage"
                     "(reader.readyState===2) `1` means Done Stage"
                 */
-                if (reader.readyState === 2) {
-                    setAvatarPreview(reader.result);
-                    setAvatar(reader.result);
-                }
-            };
-
-            // Image file save
-            reader.readAsDataURL(e.target.files[0]);
-
+        if (reader.readyState === 2) {
+          setAvatarPreview(reader.result);
+          setAvatar(reader.result);
         }
-        else {
-            setUser({ ...user, [e.target.name]: e.target.value })
-        }
+      };
+
+      // Image file save
+      reader.readAsDataURL(e.target.files[0]);
+    } else {
+      setUser({ ...user, [e.target.name]: e.target.value });
+    }
+  };
+
+  const redirect = location.search ? location.search.split("=")[1] : "/account";
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
     }
 
-   
+    if (isAuthenticated) {
+      history.push(redirect);
+    }
+  }, [dispatch, error, alert, history, isAuthenticated, redirect]);
 
-    // This code Use for React Router Dom V5
-    const redirect = location.search ? location.search.split("=")[1] : "/account";
-
-    // This code Use for React Router Dom V6
-    // const location = useLocation();
-    // const queryParams = new URLSearchParams(location.search);
-    // const redirect = queryParams.get('/shipping') || '/account';
-    // const redirect = queryParams.get('/account') || '/shipping';
-
-
-    useEffect(() => {
-
-        if (error) {
-            alert.error(error);
-            dispatch(clearErrors());
-        }
-
-
-        if (isAuthenticated) {
-            history.push(redirect);
-        }
-
-
-    }, [dispatch, error, alert, history, isAuthenticated, redirect]);
-
-
-
-    const switchTabs = (e, tab) => {
-        if (tab === "login") {
-            switchTab.current.classList.add("shiftToNeutral");
-            switchTab.current.classList.remove("shiftToRight");
-            registerTab.current.classList.remove("shiftToNeutralForm");
-            loginTab.current.classList.remove("shiftToLeft");
-        }
-
-        // opposite of login
-        if (tab === "register") {
-            switchTab.current.classList.add("shiftToRight");
-            switchTab.current.classList.remove("shiftToNeutral");
-            registerTab.current.classList.add("shiftToNeutralForm");
-            loginTab.current.classList.add("shiftToLeft");
-        }
+  const switchTabs = (e, tab) => {
+    if (tab === "login") {
+      switchTab.current.classList.add("shiftToNeutral");
+      switchTab.current.classList.remove("shiftToRight");
+      registerTab.current.classList.remove("shiftToNeutralForm");
+      loginTab.current.classList.remove("shiftToLeft");
     }
 
+    // opposite of login
+    if (tab === "register") {
+      switchTab.current.classList.add("shiftToRight");
+      switchTab.current.classList.remove("shiftToNeutral");
+      registerTab.current.classList.add("shiftToNeutralForm");
+      loginTab.current.classList.add("shiftToLeft");
+    }
+  };
 
-    return (
-        <>
-            {loading ? <Loader /> : (
-                <Fragment>
-                    <div className="LoginSignUpContainer">
-                        <div className="LoginSignUpBox">
-                            <div>
-                                <div className="login_signUp_toggle">
-                                    <p onClick={(e) => switchTabs(e, "login")}>LOGIN</p>
-                                    <p onClick={(e) => switchTabs(e, "register")}>REGISTER</p>
-                                </div>
-                                <button ref={switchTab}></button>
-                            </div>
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <div className="LoginSignUpContainer">
+            <div className="LoginSignUpBox">
+              <div>
+                <div className="login_signUp_toggle">
+                  <p onClick={(e) => switchTabs(e, "login")}>LOGIN</p>
+                  <p onClick={(e) => switchTabs(e, "register")}>REGISTER</p>
+                </div>
+                <button ref={switchTab}></button>
+              </div>
 
-                            {/* Login Form */}
-                            <form className='loginForm' ref={loginTab} onSubmit={loginSubmit}>
-                                <div className="loginEmail">
-                                    <MailOutlineIcon />
-                                    <input
-                                        type="email"
-                                        placeholder="Email"
-                                        required
-                                        value={loginEmail}
-                                        onChange={(e) => setLoginEmail(e.target.value)} />
-                                </div>
+              {/* Login Form */}
+              <form className="loginForm" ref={loginTab} onSubmit={loginSubmit}>
+                <div className="loginEmail">
+                  <MailOutlineIcon />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    required
+                    value={loginEmail}
+                    onChange={(e) => setLoginEmail(e.target.value)}
+                  />
+                </div>
 
-                                <div className="loginPassword">
-                                    <LockOpenIcon />
-                                    <input
-                                        type="password"
-                                        placeholder="Password"
-                                        required
-                                        value={loginPassword}
-                                        onChange={(e) => setLoginPassword(e.target.value)}
-                                    />
-                                </div>
-                                <Link to="/password/forgot" className="linkForgotPassword">Forgot Password?</Link>
-                                <input type="submit" value="Login" className="loginBtn" />
-                            </form>
+                <div className="loginPassword">
+                  <LockOpenIcon />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={loginPassword}
+                    onChange={(e) => setLoginPassword(e.target.value)}
+                  />
+                </div>
+                <Link to="/password/forgot" className="linkForgotPassword">
+                  Forgot Password?
+                </Link>
+                <input type="submit" value="Login" className="loginBtn" />
+              </form>
 
-                            {/* Register Form */}
-                            <form
-                                className="signUpForm"
-                                ref={registerTab}
-                                encType="multipart/form-data"
-                                onSubmit={registerSubmit}
-                            >
-                                <div className="signUpName">
-                                    <FaceIcon />
-                                    <input
-                                        type="text"
-                                        placeholder="Name"
-                                        required
-                                        name="name"
-                                        value={name}
-                                        onChange={registerDataChange}
-                                    />
-                                </div>
-                                <div className="signUpEmail">
-                                    <MailOutlineIcon />
-                                    <input
-                                        type="email"
-                                        placeholder="Email"
-                                        required
-                                        name="email"
-                                        value={email}
-                                        onChange={registerDataChange}
-                                    />
-                                </div>
-                                <div className="signUpPassword">
-                                    <LockOpenIcon />
-                                    <input
-                                        type="password"
-                                        placeholder="Password"
-                                        required
-                                        name="password"
-                                        value={password}
-                                        onChange={registerDataChange}
-                                    />
-                                </div>
+              {/* Register Form */}
+              <form
+                className="signUpForm"
+                ref={registerTab}
+                encType="multipart/form-data"
+                onSubmit={registerSubmit}
+              >
+                <div className="signUpName">
+                  <FaceIcon />
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    required
+                    name="name"
+                    value={name}
+                    onChange={registerDataChange}
+                  />
+                </div>
+                <div className="signUpEmail">
+                  <MailOutlineIcon />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    required
+                    name="email"
+                    value={email}
+                    onChange={registerDataChange}
+                  />
+                </div>
+                <div className="signUpPassword">
+                  <LockOpenIcon />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    required
+                    name="password"
+                    value={password}
+                    onChange={registerDataChange}
+                  />
+                </div>
 
-                                <div id="registerImage">
-                                    <img src={avatarPreview} alt="Avatar Preview" />
-                                    <input
-                                        type="file"
-                                        name="avatar"
-                                        accept="image/*"
-                                        onChange={registerDataChange}
-                                    />
-                                </div>
-                                <input type="submit" value="Register" className="signUpBtn" />
-                            </form>
-                        </div>
-                    </div>
-                </Fragment>
-            )}
-        </>
-    )
+                <div id="registerImage">
+                  <p style={{ color: "red", fontFamily: "roboto" }}>
+                    Image size under 50kb
+                  </p>
+                  <img src={avatarPreview} alt="Avatar Preview" />
+                  <input
+                    type="file"
+                    name="avatar"
+                    accept="image/*"
+                    onChange={registerDataChange}
+                  />
+                </div>
+                <input type="submit" value="Register" className="signUpBtn" />
+              </form>
+            </div>
+          </div>
+        </Fragment>
+      )}
+    </>
+  );
 }
 
-export default LoginSignUp
+export default LoginSignUp;
--- frontend/src/components/layout/About/About.jsx
+++ frontend/src/components/layout/About/About.jsx
@@ -4,54 +4,51 @@ import { Button, Typography, Avatar } from "@material-ui/core";
 import LinkedIn from "@material-ui/icons/LinkedIn";
 import GitHub from "@material-ui/icons/GitHub";
 
-
 function About() {
+  const visitInstagram = () => {
+    window.location = "https://github.com/sushil7276";
+  };
 
-    const visitInstagram = () => {
-        window.location = "https://github.com/sushil7276";
-    };
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
 
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              src="https://res.cloudinary.com/dw8z8ud3z/image/upload/v1690881261/s6_ehbuit.jpg"
+              alt="Founder"
+            />
+            <Typography>Sushil Babar</Typography>
+            <Button onClick={visitInstagram} color="primary">
+              Visit GitHub
+            </Button>
+            <span>
+              This is a sample website made by @sushilbabar. Only with the
+              purpose to learn MERN Stack
+            </span>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Profile</Typography>
+            <a
+              href="https://www.linkedin.com/in/sushil-babar-3a6618237/"
+              target="blank"
+            >
+              <LinkedIn className="youtubeSvgIcon" />
+            </a>
 
-    return (
-        <div className="aboutSection">
-            <div></div>
-            <div className="aboutSectionGradient"></div>
-            <div className="aboutSectionContainer">
-                <Typography component="h1">About Us</Typography>
-
-                <div>
-                    <div>
-                        <Avatar
-                            style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
-                            src="https://res.cloudinary.com/dw8z8ud3z/image/upload/v1690377064/1673263815827_uurasw.jpg"
-                            alt="Founder"
-                        />
-                        <Typography>Sushil Babar</Typography>
-                        <Button onClick={visitInstagram} color="primary">
-                            Visit GitHub
-                        </Button>
-                        <span>
-                            This is a sample website made by @sushilbabar. Only with the
-                            purpose to learn MERN Stack
-                        </span>
-                    </div>
-                    <div className="aboutSectionContainer2">
-                        <Typography component="h2">Our Profile</Typography>
-                        <a
-                            href="https://www.linkedin.com/in/sushil-babar-3a6618237/"
-                            target="blank"
-                        >
-                            <LinkedIn className="youtubeSvgIcon" />
-                        </a>
-
-                        <a href="https://github.com/sushil7276" target="blank">
-                            <GitHub className="instagramSvgIcon" />
-                        </a>
-                    </div>
-                </div>
-            </div>
+            <a href="https://github.com/sushil7276" target="blank">
+              <GitHub className="instagramSvgIcon" />
+            </a>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
